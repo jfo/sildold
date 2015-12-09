1af9a283d1b9ac5515876ba9abeb74957830cd79
urthbound <jeffowler@gmail.com>
2015-12-08 23:00:43 -0500

    will never explicitly create nil, typdef refactor

--- cell.c
+++ cell.c
@@ -8,25 +8,19 @@ union value {
     struct cell * list;
 };
 
-struct cell {
+typedef struct cell {
     int type;
     union value value;
     struct cell * next;
-};
-
-typedef struct cell cell;
+} cell;
 
 cell nil = { NIL, { 0x0 }, 0x0 };
 cell truth = { LABEL, {"#t"}, &nil };
 
 cell* makecell(int type, union value val, cell * next) {
-    if (type == NIL) {
-        return &nil;
-    } else {
-        cell* output = malloc(sizeof(cell));
-        output->type = type;
-        output->value = val;
-        output->next = next;
-        return output;
-    }
+    cell* output = malloc(sizeof(cell));
+    output->type = type;
+    output->value = val;
+    output->next = next;
+    return output;
 }
