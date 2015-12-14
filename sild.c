545099219cf54ec26f3de5d9aaf728cffb068d6d
urthbound <jeffowler@gmail.com>
2015-12-14 10:13:09 -0500

    apply

--- sild.c
+++ sild.c
@@ -7,33 +7,80 @@
 #include "debugging.c"
 #include "builtins.c"
 
+cell*apply(cell*);
+
 cell* eval(cell *c, cell* dict) {
-    if (c->type == LIST && strcmp(c->value.list->value.label, "quote") == 0) {
-        cell *r = quote(c->value.list->next);
-        r->next = eval(c->next, dict);
-        c = r;
-        return c;
+    if (c == &nil) {
+        return &nil;
+    } else if (c->type == LIST && strcmp(c->value.list->value.label, "quote") == 0) {
+        cell *out = quote(c->value.list->next);
+        out->next = eval(c->next, dict);
+        return out;
     } else if (c->type == LIST) {
-        c->value.list = eval(c->value.list, dict);
-        c->next = eval(c->next, dict);
-        return c;
+        cell* out;
+        out = makecell(LIST,(value) { .list = eval(c->value.list, dict)}, &nil);
+        out = apply(out);
+        out->next = eval(c->next, dict);
+        return out;
     } else if (c->type == LABEL) {
         cell *val = assoc(c->value.label, dict);
+        cell *out;
         if (val != &nil) {
-            c = val;
+             out = val;
+        } else {
+            printf("Unbound variable: %s\n", c->value.label);
+            exit(1);
         }
-        c->next = eval(c->next, dict);
-        return c;
+        out->next = eval(c->next, dict);
+        return out;
     }
-    return c;
+    return &nil;
+}
+
+cell* apply(cell *n) {
+    cell* operator = n->value.list;
+    cell* first_operand = n->value.list->next;
+
+    if (strcmp(operator->value.label, "quote") == 0) {
+        return quote(first_operand);
+    /* } else if (strcmp(operator->value.label, "label") == 0) { */
+    /*     return label(first_operand); */
+    } else if (strcmp(operator->value.label, "eq") == 0) {
+        return eq(first_operand);
+    } else if (strcmp(operator->value.label, "car") == 0) {
+        return car(first_operand);
+    } else if (strcmp(operator->value.label, "cdr") == 0) {
+        return cdr(first_operand);
+    } else if (strcmp(operator->value.label, "cons") == 0) {
+        return cons(first_operand);
+    /* } else if (strcmp(operator->value.label, "cond") == 0) { */
+    /*     return cond(first_operand); */
+    /* } else if (strcmp(operator->value.label, "label") == 0) { */
+    /*     add_label(first_operand->value.label, copy_cell(first_operand->next)); */
+    /*     return find_label(first_operand->value.label); */
+    /* } else if (strcmp(operator->value.label, "lambda") == 0) { */
+    /*     return quote(n); */
+    /* } else if (operator->type == LIST && strcmp(operator->value.list->value.label, "lambda") == 0) { */
+    /*     return eval(lambda(n)); */
+    } else if (operator->type == LIST) {
+    } else {
+        printf("Attempted to apply non-procedure \"%s\"", operator->value.label);
+        exit(1);
+    }
+
+    return &nil;
 }
 
 int main() {
 
     /* char* test = "((that 7) (this 6))"; */
-    char* test = "((tis 6) (this 8)(that x) (tat thing))";
-    cell* list = read(&test);
-    debuglist(eval(list, list));
+    char* test = "((cons cons) (car car) (cdr cdr))";
+    cell* testdict = read(&test);
+
+    /* char* testl = "(cons 'this '(taht))"; */
+    char* testl = "(cdr (cdr (car (cdr '(this that)))))";
+    cell* testlist = read(&testl);
+    debuglist(eval(testlist, testdict));
 
     return 0;
 }
