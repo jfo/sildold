9a670664530ba6aec8d6219d522d4dbb4d4db020
urthbound <jeffowler@gmail.com>
2015-12-13 21:52:53 -0500

    eval

--- sild.c
+++ sild.c
@@ -7,12 +7,33 @@
 #include "debugging.c"
 #include "builtins.c"
 
+cell* eval(cell *c, cell* dict) {
+    if (c->type == LIST && strcmp(c->value.list->value.label, "quote") == 0) {
+        cell *r = quote(c->value.list->next);
+        r->next = eval(c->next, dict);
+        c = r;
+        return c;
+    } else if (c->type == LIST) {
+        c->value.list = eval(c->value.list, dict);
+        c->next = eval(c->next, dict);
+        return c;
+    } else if (c->type == LABEL) {
+        cell *val = assoc(c->value.label, dict);
+        if (val != &nil) {
+            c = val;
+        }
+        c->next = eval(c->next, dict);
+        return c;
+    }
+    return c;
+}
+
 int main() {
 
     /* char* test = "((that 7) (this 6))"; */
-    char* test = "((this 6) (that x) (tat thing))";
+    char* test = "((tis 6) (this 8)(that x) (tat thing))";
     cell* list = read(&test);
-    debuglist(assoc("tat", list));
+    debuglist(eval(list, list));
 
     return 0;
 }
